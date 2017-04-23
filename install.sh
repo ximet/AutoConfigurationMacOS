1a276971887240c5dcd0edd4d5ebc48c5d007778
ximet <dq.ximet@gmail.com>
2017-04-23 12:17:17 +0300

    install node js

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+#install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 brew update;
+
+#install node js
+curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
