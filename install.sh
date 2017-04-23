709b7e6341e55f97756c517e1a26a9b7b8d908ae
ximet <dq.ximet@gmail.com>
2017-04-23 12:19:17 +0300

    install docker

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ brew update;
 
 #install node js
 curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+
+#install docker
+curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose -o /usr/local/etc/bash_completion.d/docker-compose
